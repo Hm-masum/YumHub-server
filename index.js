d9a75135ce6d5ca47edf0c50d2ef8af6b543d222
Hm-masum <hhmasum88@gmail.com>
2024-05-11 23:22:05 +0600

    jwt generate commit

--- index.js
+++ index.js
@@ -1,7 +1,8 @@
 const express = require('express');
 const cors = require('cors');
 require('dotenv').config()
-
+const jwt=require('jsonwebtoken')
+const cookieParser=require('cookie-parser')
 const { MongoClient, ServerApiVersion, ObjectId } = require('mongodb');
 const port = process.env.PORT || 5000;
 const app = express()
@@ -13,7 +14,24 @@ const corsOptions={
 }
 app.use(cors(corsOptions))
 app.use(express.json())
+app.use(cookieParser())
 
+// verify jwt middleware
+const verifyToken=(req,res,next)=>{
+    const token = req.cookies?.token;
+    if(!token){
+      return res.status(401).send({message:'unauthorized access'})
+    }
+    if(token){
+      jwt.verify(token.process.env.ACCESS_TOKEN_SECRET,(err,decoded)=>{
+        if(err){
+          return res.status(403).send({message:'forbidden access'})
+        }
+        req.user=decoded;
+        next()
+      })
+    }
+}
 
 
 const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}@cluster0.cxuuz57.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0`;
@@ -31,6 +49,31 @@ async function run() {
   try { 
 
     const foodsCollection=client.db('YumHub').collection('foods')
+
+        // jwt generate
+    app.post('/jwt',async(req,res)=>{
+        const email=req.body;
+        const token=jwt.sign(email,process.env.ACCESS_TOKEN_SECRET,{
+            expiresIn:'7d'
+        })
+        res.cookie('token',token,{
+            httpOnly:true,
+            secure: process.env.NODE_ENV==='production',
+            sameSite: process.env.NODE_ENV==='production' ? 'none' : 'strict'
+        }).send({success: true})
+    })
+      
+    app.get('/logout',(req,res)=>{
+      res.
+      clearCookie('token',{
+         httpOnly:true,
+         secure: process.env.NODE_ENV==='production',
+         sameSite: process.env.NODE_ENV==='production' ? 'none' : 'strict',
+         maxAge:0,
+      })
+       .send({success: true})
+   })
+      
 
     app.post('/food',async(req,res)=>{
         const foodData=req.body;
